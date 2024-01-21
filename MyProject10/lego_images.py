5ce742683455e1284bdb93f17e3beeaa86819af8
canacar <143259813+CodeJourneyCAN@users.noreply.github.com>
2024-01-21 21:23:24 +0300

    Update lego_images.py

--- MyProject10/lego_images.py
+++ MyProject10/lego_images.py
@@ -2,17 +2,17 @@
 from color_definitions import color_ranges
 from color_definitions import dish_ratios
 lego_images = {
-    "Soup": "/Users/canacar/Desktop/lego.images/soup.lego.jpg",
-    "Cheese Platter": "/Users/canacar/Desktop/lego.images/cheese.lego.jpg",
-    "Garlic": "/Users/canacar/Desktop/lego.images/garlic.lego.jpg",
-    "Chicken": "/Users/canacar/Desktop/lego.images/chicken.lego.jpg",
-    "Fish": "/Users/canacar/Desktop/lego.images/fish.lego.jpg",
-    "Omelet": "/Users/canacar/Desktop/lego.images/omelet.lego.jpg",
-    "Meatballs": "/Users/canacar/Desktop/lego.images/meatballs.lego.jpg",
-    "Casserole": "/Users/canacar/Desktop/lego.images/casserole.lego.jpg",
-    "Fajitas": "/Users/canacar/Desktop/lego.images/fajitas.lego.jpg",
-    "Souffle": "/Users/canacar/Desktop/lego.images/souffle.lego.jpg",
-    "Tiramisu": "/Users/canacar/Desktop/lego.images/tiramisu.lego.jpg",
-    "Cheesecake": "/Users/canacar/Desktop/lego.images/cheesecake.lego.jpg",
+    "Soup": "/Users/canacar/Desktop/lego.images/Soup.jpg",
+    "Cheese Platter": "/Users/canacar/Desktop/lego.images/Cheese_Platter.jpg",
+    "Garlic": "/Users/canacar/Desktop/lego.images/Garlic.jpg",
+    "Chicken": "/Users/canacar/Desktop/lego.images/Chicken.jpg",
+    "Fish": "/Users/canacar/Desktop/lego.images/Fish.jpg",
+    "Omelet": "/Users/canacar/Desktop/lego.images/Omelet.jpg",
+    "Meatballs": "/Users/canacar/Desktop/lego.images/Meatballs.jpg",
+    "Casserole": "/Users/canacar/Desktop/lego.images/Casserole.jpg",
+    "Fajitas": "/Users/canacar/Desktop/lego.images/Fajitas.jpg",
+    "Souffle": "/Users/canacar/Desktop/lego.images/Souffle.jpg",
+    "Tiramisu": "/Users/canacar/Desktop/lego.images/Tiramisu.jpg",
+    "Cheesecake": "/Users/canacar/Desktop/lego.images/Cheesecake.jpg",
 
 }
