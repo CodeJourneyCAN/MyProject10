c2626c274b664e714f20df021757bd1f60238757
canacar <143259813+CodeJourneyCAN@users.noreply.github.com>
2024-01-21 21:25:19 +0300

    Update real_menu.py

--- MyProject10/real_menu.py
+++ MyProject10/real_menu.py
@@ -8,16 +8,16 @@ class RealDish:
 
 # Gerçek menüyü oluşturun
 real_menu = [
-    RealDish("Soup", "green", "square", 15, "/Users/canacar/Desktop/lego.images/soup.jpg"),
-    RealDish("Cheese Platter", "green", "rectangle", 20, "/Users/canacar/Desktop/lego.images/cheese.jpg"),
-    RealDish("Garlic", "green", "rectangle", 15, "/Users/canacar/Desktop/lego.images/garlic.jpg"),
-    RealDish("Chicken", "red", "square", 20, "/Users/canacar/Desktop/lego.images/chicken.jpg"),
-    RealDish("Fish", "red", "rectangle", 20, "/Users/canacar/Desktop/lego.images/fish.jpg"),
-    RealDish("Omelet", "red", "rectangle", 20, "/Users/canacar/Desktop/lego.images/omelet.jpg"),
-    RealDish("Meatballs", "blue", "square", 20, "/Users/canacar/Desktop/lego.images/meatballs.jpg"),
-    RealDish("Casserole", "blue", "rectangle", 20, "/Users/canacar/Desktop/lego.images/casserole.jpg"),
-    RealDish("Fajitas", "blue", "rectangle", 20, "/Users/canacar/Desktop/lego.images/fajitas.jpg"),
-    RealDish("Souffle", "yellow", "square", 20, "/Users/canacar/Desktop/lego.images/souffle.jpg"),
-    RealDish("Tiramisu", "yellow", "rectangle", 20, "/Users/canacar/Desktop/lego.images/tiramisu.jpg"),
-    RealDish("Cheesecake", "yellow", "rectangle", 20, "/Users/canacar/Desktop/lego.images/cheesecake.jpg"),
+    RealDish("Soup", "green", "square", 15, "/Users/canacar/Desktop/lego.images/Soup.jpg"),
+    RealDish("Cheese Platter", "green", "rectangle", 20, "/Users/canacar/Desktop/lego.images/Cheese_Platter.jpg"),
+    RealDish("Garlic", "green", "rectangle", 15, "/Users/canacar/Desktop/lego.images/Garlic.jpg"),
+    RealDish("Chicken", "red", "square", 20, "/Users/canacar/Desktop/lego.images/Chicken.jpg"),
+    RealDish("Fish", "red", "rectangle", 20, "/Users/canacar/Desktop/lego.images/Fish.jpg"),
+    RealDish("Omelet", "red", "rectangle", 20, "/Users/canacar/Desktop/lego.images/Omelet.jpg"),
+    RealDish("Meatballs", "blue", "square", 20, "/Users/canacar/Desktop/lego.images/Meatballs.jpg"),
+    RealDish("Casserole", "blue", "rectangle", 20, "/Users/canacar/Desktop/lego.images/Casserole.jpg"),
+    RealDish("Fajitas", "blue", "rectangle", 20, "/Users/canacar/Desktop/lego.images/Fajitas.jpg"),
+    RealDish("Souffle", "yellow", "square", 20, "/Users/canacar/Desktop/lego.images/Souffle.jpg"),
+    RealDish("Tiramisu", "yellow", "rectangle", 20, "/Users/canacar/Desktop/lego.images/Tiramisu.jpg"),
+    RealDish("Cheesecake", "yellow", "rectangle", 20, "/Users/canacar/Desktop/lego.images/Cheesecake.jpg"),
 ]
